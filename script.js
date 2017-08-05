fca25b1df47097f8d94e0c317304f28a628b5be5
iuliya <hiwaluna@yandex.ru>
2017-08-05 20:40:48 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var showList,
     category = document.getElementsByName("category"),
     classification = document.getElementsByName("classification"),
     btn_result = document.getElementById("btn_result"),
+    btn_comment = document.getElementById("btn_comment"),
     inputs = document.getElementsByTagName("input"),
     err = document.querySelector(".error"),
-    idList = oldList = "";
+    count = document.querySelector("#count input[type=text]"),
+    idList = oldList = tprList = "";
 
     isCheck(direction);
     isCheck(system);
@@ -21,20 +23,34 @@ function isCheck(arr, elem){
 
       if (this.value == "tpr"){
         document.getElementById("btn_comment").classList.remove("invisible");
+        document.getElementById("classification").classList.add("invisible");
+        document.getElementById("count").classList.remove("invisible");
       }
   });
 
 }
 
+count.addEventListener("change", function(){
+  console.log(count.value);
+});
+
 btn_result.addEventListener("click", function(){
+  if (!idList) idList = "sasp";
   var list = document.getElementById(idList);
+  //var count = 1;
   if (list){
     document.querySelector(".information").style.backgroundPosition = "-1000px";
-    list.style.display = "block";
-    oldList =idList;
-  } else {
-      err.style.display="block";
+    var n = count.value;
+    if (n) {
+      tprList =  list.innerText;
+      list.innerText = tprList.replace(/:х./g, ":" + n + ".");
+      list.style.display = "block";
+
+    } else {
+      list.style.display = "block";
+    }
   }
+    oldList =idList;
 });
 
 btn_reset.addEventListener("click", function(){
@@ -42,6 +58,7 @@ btn_reset.addEventListener("click", function(){
   var list = document.getElementById(oldList);
   if (oldList){
       list.style.display = "none";
+      list.innerText = tprList;
       document.querySelector(".information").style.backgroundPosition = "center"
   } else{
       err.style.display="none";
@@ -51,4 +68,12 @@ btn_reset.addEventListener("click", function(){
     inputs[i].removeAttribute("checked");
 
     document.getElementById("btn_comment").classList.add("invisible");
+    document.getElementById("classification").classList.remove("invisible");
+    document.getElementById("count").classList.add("invisible");
+    document.getElementById("comments").style.display = "none";
+
+});
+
+btn_comment.addEventListener("click", function(){
+  document.getElementById("comments").style.display = "block";
 });
