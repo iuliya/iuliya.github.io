72bb34efa1595025e6b320636f04e6aac97e512b
iuliya <hiwaluna@yandex.ru>
2017-08-05 16:02:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,82 +4,51 @@ var showList,
     category = document.getElementsByName("category"),
     classification = document.getElementsByName("classification"),
     btn_result = document.getElementById("btn_result"),
-    inputs = document.getElementsByTagName("input");
+    inputs = document.getElementsByTagName("input"),
+    err = document.querySelector(".error"),
+    idList = oldList = "";
 
-    var idList = oldList = "";
+    isCheck(direction);
+    isCheck(system);
+    isCheck(category);
+    isCheck(classification);
 
 function isCheck(arr, elem){
-
   for (var i = 0; i < arr.length; i++)
-  arr[i].addEventListener("change", function(){
-    this.setAttribute("checked", true);
-    idList = idList + this.value;
+    arr[i].addEventListener("change", function(){
+      this.setAttribute("checked", true);
+      idList = idList + this.value;
 
+      if (this.value == "tpr"){
+        document.getElementById("btn_comment").classList.remove("invisible");
+      }
   });
 
 }
 
-isCheck(direction);
-isCheck(system);
-isCheck(category);
-isCheck(classification);
-
-
 btn_result.addEventListener("click", function(){
-
- console.log(idList);
-var e = document.getElementById(idList);
-e.style.display = "block";
-  console.log(e);
-  oldList =idList;
+  var list = document.getElementById(idList);
+  if (list){
+    document.querySelector(".information").style.backgroundPosition = "-1000px";
+    list.style.display = "block";
+    oldList =idList;
+  } else {
+      err.style.display="block";
+  }
 });
 
 btn_reset.addEventListener("click", function(){
   idList = "";
-for (var i = 0; i < inputs.length; i++)
- inputs[i].removeAttribute("checked");
+  var list = document.getElementById(oldList);
+  if (oldList){
+      list.style.display = "none";
+      document.querySelector(".information").style.backgroundPosition = "center"
+  } else{
+      err.style.display="none";
+      console.log("была ошибка ");
+  }
+  for (var i = 0; i < inputs.length; i++)
+    inputs[i].removeAttribute("checked");
 
-var e = document.getElementById(oldList);
-e.style.display = "none";
-console.log("текущий элемент " + idList)
+    document.getElementById("btn_comment").classList.add("invisible");
 });
-
-/*
-btn_result.addEventListener("click", function(){
-  itemDirection = hasChecked(direction);
-  console.log(itemDirection + direction[itemDirection].value);
-
-  if (direction[itemDirection].value == "tpr")  {
-    for(var i = 0; i < classification.length; i++) classification[i].setAttribute("disabled", true);
-  }
-
-  itemSystem = hasChecked(system);
-  console.log(itemSystem);
-  itemCategory = hasChecked(category);
-  console.log(itemCategory);
-  itemClassification = hasChecked(classification);
-  console.log(itemClassification);
-  var tmp = direction[itemDirection].value + "_" + system[itemSystem].value + "_" +category[itemCategory].value;
-
-  if (system[itemSystem].value == "lira" || system[itemSystem].value == "eissd")
-  {
-
-    tmp = tmp + "_" + classification[itemClassification].value;
-  }
-
-
-  if (showList) {
-    document.getElementById(showList).style.display = "none";
-  }
-  showList = tmp;
-  document.getElementById(showList).style.display = "block";
-
-  console.log(tmp);
-  console.log(showList);
-});
-
-function hasChecked (arr){
-  for(var i = 0; i < arr.length; i++)
-  if (arr[i].hasAttribute("checked"))  return i;
-  }
-*/
